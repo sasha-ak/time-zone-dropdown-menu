2238cd3d31691da767c5393ecae967a46ea44e05
sasha-ak <157721905+sasha-ak@users.noreply.github.com>
2024-04-09 16:40:38 +0200

    Added event listener to each city in dropdown menu

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 function showSelectedCountry(event) {
-  if (event.target.value.length > 0) {
-    alert(event.target.value);
+  if (event.target.value === "paris") {
+    alert(
+      `It is currently ${moment()
+        .tz(`Europe/Paris`)
+        .format(`dddd, MMMM D, YYYY h:mm A`)} in Europe/Paris`
+    );
+  }
+  if (event.target.value === "sydney") {
+    alert(
+      `It is currently ${moment()
+        .tz(`Australia/Sydney`)
+        .format(`dddd, MMMM D, YYYY h:mm A`)} in Australia/Sydney`
+    );
+  }
+  if (event.target.value === "tokyo") {
+    alert(
+      `It is currently ${moment()
+        .tz(`Asia/Tokyo`)
+        .format(`dddd, MMMM D, YYYY h:mm A`)} in Asia/Tokyo`
+    );
   }
 }
 
